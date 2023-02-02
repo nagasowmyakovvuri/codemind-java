9f9835b65c62b9ffc9977bcf33047afa09b1485b
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2023-02-02 21:14:16 +0530

    import java.util.*; class A{   public static void main(String args[]) {     Scanner s=new Scanner(System.in);     int a,b;     a=s.nextInt();     int o;     o=(a*(a-1))/2;     System.out.println(o);
    }
    };

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class A{
+  public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in);
+    int a,b;
+    a=s.nextInt();
+    int o;
+    o=(a*(a-1))/2;
+    System.out.println(o);
+    
+}
+};
