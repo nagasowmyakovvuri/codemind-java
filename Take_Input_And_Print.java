e8921a38812af76bc20361d2f2d34b25d99a8749
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2023-02-02 21:09:07 +0530

    import java.util.*; class A{   public static void main(String args[]) {     Scanner s=new Scanner(System.in);     String d;     d=s.nextLine();     System.out.println(d); } };

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class A{
+  public static void main(String args[])
+{
+    Scanner s=new Scanner(System.in);
+    String d;
+    d=s.nextLine();
+    System.out.println(d);
+}
+};
