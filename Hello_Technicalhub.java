9d694e7c5b7cd840c74a38845768d87983494ccf
nagasowmyakovvuri <97426041+nagasowmyakovvuri@users.noreply.github.com>
2023-02-03 09:37:20 +0530

    import java.util.*; class A{   public static void main(String args[])   {       Scanner s=new Scanner(System.in);       String n;       n=s.nextLine();       System.out.println("Hello Technicalhub");       System.out.println(n);   } };

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class A{
+  public static void main(String args[])
+  {
+      Scanner s=new Scanner(System.in);
+      String n;
+      n=s.nextLine();
+      System.out.println("Hello Technicalhub");
+      System.out.println(n);
+  }
+};
